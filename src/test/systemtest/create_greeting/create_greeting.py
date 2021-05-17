0da1fe2126865fcc0bace64d58077e2807c4c339
Arne Christian Danbolt Nilsen <Arne.Nilsen@skatteetaten.no>
2021-05-17 07:34:02 +0200

    Want to be able to run with h2 in developer ide

--- src/test/systemtest/create_greeting/create_greeting.py
+++ src/test/systemtest/create_greeting/create_greeting.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     #melding = input("Skriv inn melding: ")
     #navn = input("Hilsen: ")
 
-    with req.post("http://127.0.0.1:55490/greeting/",  verify=False,headers= {'Content-Type': 'application/json', 'Accept': 'text/plain'},
+    with req.post("http://127.0.0.1:8080/greeting/",  verify=False,headers= {'Content-Type': 'application/json', 'Accept': 'text/plain'},
                   json= {'to':'john', 'message': '{}'.format("Merry christmas"), 'from':'santa'.format("navn")}) as response:
         print(response.status_code)
--- src/test/systemtest/create_greeting/list_greetings.py
+++ src/test/systemtest/create_greeting/list_greetings.py
@@ -3,6 +3,6 @@
 import requests as req
 
 if __name__ == "__main__":
-    with req.get("http://127.0.0.1:55490/greeting",  verify=False) as response:
+    with req.get("http://127.0.0.1:8080/greeting",  verify=False) as response:
         print(response.text)
 
